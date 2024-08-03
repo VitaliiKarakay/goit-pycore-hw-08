dbf3dd5faf72443ebec652fb03b1b9f935fdb8db
v.karakai <v.karakai@pin-up.tech>
2024-08-03 11:16:11 +0300

    [HW-8] - added save and load address book

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from src.address_book.address_book import AddressBook
 from src.bot.commands import parse_input
 from src.bot.handlers import handle_invalid_command, command_handlers
+from src.utils.save_load import load_data, save_data
 
 
 def main():
-    contacts = AddressBook()
+    contacts = load_data()
 
     print("Welcome to the assistant bot!")
     print("Input 'help' for a list of commands.")
@@ -13,6 +14,7 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
         handler = command_handlers.get(command, handle_invalid_command)
+        save_data(book=contacts)
         should_exit = handler(args, contacts)
 
 
